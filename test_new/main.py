2ea15b10c8d3e360a20d8303ec56d28c77f01f2c
Niklas/Desktop <niklasschemmer@googlemail.com>
2022-09-26 19:05:17 +0200

    Fix action selection error Add pseudocode description

--- test_new/main.py
+++ test_new/main.py
@@ -14,51 +14,22 @@ from ppo_network import PPO_Network_actor, PPO_Network_critic
 import os
 
 FLAGS = flags.FLAGS
-flags.DEFINE_bool("training", True, "Whether to train agents.")
-flags.DEFINE_bool("continuation", False, "Continuously training.")
-flags.DEFINE_float("learning_rate", 5e-4, "Learning rate for training.")
-flags.DEFINE_float("discount", 0.99, "Discount rate for future rewards.")
-flags.DEFINE_integer("max_steps", int(1e5), "Total steps for training.")
-flags.DEFINE_integer("snapshot_step", int(1e3), "Step for snapshot.")
-flags.DEFINE_string("snapshot_path", "./snapshot/", "Path for snapshot.")
-flags.DEFINE_string("log_path", "./log/", "Path for log.")
-flags.DEFINE_string("device", "1", "Device for training.")
-
-flags.DEFINE_string("map", "DefeatZerglingsAndBanelings", "Name of a map to use.")
-flags.DEFINE_bool("render", True, "Whether to render with pygame.")
+flags.DEFINE_integer("steps_per_epoch", 300, "How many steps to do while one epoch iteration.")
+flags.DEFINE_integer("epochs", 1000, "Number of epochs for training.")
+flags.DEFINE_float("gamma", 0.99, "Discount value for future rewards.")
+flags.DEFINE_float("clip_ratio", 0.2, "Discount value for future rewards.")
+flags.DEFINE_float("policy_learning_rate", 7e-6, "Learning rate for the policy function.")
+flags.DEFINE_float("value_function_learning_rate", 1e-5, "Learning rate for the value funtion.")
+flags.DEFINE_integer("train_policy_iterations", 75, "Max training iterations for policy funtion.")
+flags.DEFINE_integer("train_value_iterations", 75, "Training iterations for value funtion.")
+flags.DEFINE_float("lam", 0.9, "Smoothing parameter used for reducing variance.")
+flags.DEFINE_float("target_kl", 0.01, "Target Kullback Leibler divergence to stop at if reached while policy update.")
+flags.DEFINE_integer("step_mul", 8, "Game steps per agent step.")
 flags.DEFINE_integer("screen_resolution", 64, "Resolution for screen feature layers.")
 flags.DEFINE_integer("minimap_resolution", 64, "Resolution for minimap feature layers.")
-flags.DEFINE_integer("step_mul", 8, "Game steps per agent step.")
-
-flags.DEFINE_string("agent", "agents.a3c_agent.A3CAgent", "Which agent to run.")
-flags.DEFINE_string("net", "fcn", "atari or fcn.")
 flags.DEFINE_integer("max_agent_steps", 120, "Total agent steps.")
 
-flags.DEFINE_bool("profile", False, "Whether to turn on code profiling.")
-flags.DEFINE_bool("trace", False, "Whether to trace the code execution.")
-flags.DEFINE_integer("parallel", 1, "How many instances to run in parallel.")
-flags.DEFINE_bool("save_replay", False, "Whether to save a replay at the end.")
-
 FLAGS(sys.argv)
-if FLAGS.training:
-  PARALLEL = FLAGS.parallel
-  MAX_AGENT_STEPS = FLAGS.max_agent_steps
-  DEVICE = ['/cpu:0']
-else:
-  PARALLEL = 1
-  MAX_AGENT_STEPS = 1e5
-  DEVICE = ['/cpu:0']
-
-steps_per_epoch = 300
-epochs = 1000
-gamma = 0.99
-clip_ratio = 0.2
-policy_learning_rate = 7e-6
-value_function_learning_rate = 1e-5
-train_policy_iterations = 75
-train_value_iterations = 75
-lam = 0.9
-target_kl = 0.01
 
 physical_devices = tf.config.list_physical_devices('GPU')
 try:
@@ -78,22 +49,20 @@ env = sc2_env.SC2Env(
                             use_feature_units=True),
     visualize=True)
     
-buffer = Buffer((utils.screen_channel(), FLAGS.screen_resolution, FLAGS.screen_resolution), len(actions.FUNCTIONS), steps_per_epoch, lam=lam)
+buffer = Buffer((utils.screen_channel(), FLAGS.screen_resolution, FLAGS.screen_resolution), len(actions.FUNCTIONS), FLAGS.steps_per_epoch, lam=FLAGS.lam)
 
 actor = PPO_Network_actor(FLAGS)
 critic = PPO_Network_critic(FLAGS)
 
 
-policy_optimizer = keras.optimizers.Adam(learning_rate=policy_learning_rate)
-value_optimizer = keras.optimizers.Adam(learning_rate=value_function_learning_rate)
+policy_optimizer = keras.optimizers.Adam(learning_rate=FLAGS.policy_learning_rate)
+value_optimizer = keras.optimizers.Adam(learning_rate=FLAGS.value_function_learning_rate)
 
 observation = env.reset()
 episode_return = 0
 episode_length = 0
 screen = np.array(observation[0].observation['feature_screen'], dtype=np.float32)
 screen = np.expand_dims(utils.preprocess_screen(screen), axis=0)
-available_actions = np.zeros([1, len(actions.FUNCTIONS)], dtype=np.float32)
-available_actions[0, observation[0].observation['available_actions']] = 1
 
 @tf.function
 def logprobabilities(logits, a):
@@ -102,14 +71,6 @@ def logprobabilities(logits, a):
         tf.one_hot(a, logits.shape[-1]) * logprobabilities_all, axis=-1
     )
     return logprobability
-
-@tf.function
-def sample_action(screen, avail_actions):
-    non_spatial_logits, spatial_out_logits = actor(screen, avail_actions)
-
-    spatial_action = tf.squeeze(tf.random.categorical(spatial_out_logits, 1), axis=1)
-    non_spatial_action = tf.squeeze(tf.random.categorical(non_spatial_logits, 1), axis=1)
-    return non_spatial_logits, spatial_out_logits, non_spatial_action, spatial_action
 
 @tf.function
 def train_policy(screen_buffer, avail_actions_buffer, non_spatial_action_buffer, spatial_action_buffer, logprobability_buffer, advantage_buffer):
@@ -126,8 +87,8 @@ def train_policy(screen_buffer, avail_actions_buffer, non_spatial_action_buffer,
         )
         min_advantage = tf.where(
             advantage_buffer > 0,
-            (1 + clip_ratio) * advantage_buffer,
-            (1 - clip_ratio) * advantage_buffer,
+            (1 + FLAGS.clip_ratio) * advantage_buffer,
+            (1 - FLAGS.clip_ratio) * advantage_buffer,
         )
 
         non_spatial_policy_loss = -tf.reduce_mean(
@@ -153,31 +114,27 @@ def train_policy(screen_buffer, avail_actions_buffer, non_spatial_action_buffer,
     kl = tf.add(tf.reduce_sum(non_spatial_kl), tf.reduce_sum(spatial_kl))
     return kl
 
-def step(obs, screen, available_actions):
-    non_spatial_logits, spatial_out_logits, non_spatial_action, spatial_action = sample_action(screen, available_actions)
+def step(screen, available_actions, available_actions_one_hot):    
+    non_spatial_logits, spatial_out_logits = actor(screen, available_actions_one_hot)
+    spatial_action = tf.squeeze(tf.random.categorical(spatial_out_logits, 1), axis=1)
 
     # Select an action and a spatial target
     non_spatial_logits = non_spatial_logits.numpy().ravel()
     spatial_out_logits = spatial_out_logits.numpy().ravel()
-    valid_actions = obs.observation['available_actions']
-    act_id = valid_actions[np.argmax(non_spatial_logits[valid_actions])]
-    target = np.argmax(spatial_out_logits)
+    non_spatial_action = available_actions[tf.random.categorical([non_spatial_logits[available_actions]], 1)[0][0]]
+    
+    target = spatial_action.numpy()[0]
     target = [int(target // FLAGS.screen_resolution), int(target % FLAGS.screen_resolution)]
 
-    dy = np.random.randint(-4, 5)
-    target[0] = int(max(0, min(FLAGS.screen_resolution-1, target[0]+dy)))
-    dx = np.random.randint(-4, 5)
-    target[1] = int(max(0, min(FLAGS.screen_resolution-1, target[1]+dx)))
-
     act_args = []
-    for arg in actions.FUNCTIONS[act_id].args:
+    for arg in actions.FUNCTIONS[non_spatial_action].args:
       if arg.name in ('screen', 'minimap', 'screen2'):
         act_args.append([target[1], target[0]])
       else:
         act_args.append([0])
         
-    value_t = critic(screen, available_actions)
-    func_actions = actions.FunctionCall(act_id, act_args)
+    value_t = critic(screen, available_actions_one_hot)
+    func_actions = actions.FunctionCall(non_spatial_action, act_args)
 
     return func_actions, non_spatial_logits, spatial_out_logits, non_spatial_action, spatial_action, value_t
 
@@ -190,14 +147,18 @@ def train_value_function(screen_buffer, available_actions_buffer, return_buffer)
     value_optimizer.apply_gradients(zip(value_grads, critic.trainable_variables))
 
 
-for epoch in range(epochs):
+for epoch in range(FLAGS.epochs):
     sum_return = 0
     sum_length = 0
     num_episodes = 0
     num_frames = 0
 
-    for t in range(steps_per_epoch):
-        func_actions, non_spatial_logits, spatial_out_logits, non_spatial_action, spatial_action, value_t = step(observation[0], screen, available_actions)
+    for t in range(FLAGS.steps_per_epoch):
+        available_actions = observation[0].observation['available_actions']
+        available_actions_one_hot = np.zeros([1, len(actions.FUNCTIONS)], dtype=np.float32)
+        available_actions_one_hot[0, ] = 1
+
+        func_actions, non_spatial_logits, spatial_out_logits, non_spatial_action, spatial_action, value_t = step(screen, available_actions, available_actions_one_hot)
         observation_new = env.step([func_actions])
         episode_return += observation_new[0].reward
         episode_length += 1
@@ -208,19 +169,17 @@ for epoch in range(epochs):
         non_spatial_logprobability_t = logprobabilities(non_spatial_logits, non_spatial_action)
 
         # Store obs, act, rew, v_t, logp_pi_t
-        buffer.store(screen, available_actions, non_spatial_action, spatial_action, observation_new[0].reward, value_t, (non_spatial_logprobability_t, spatial_logprobability_t))
+        buffer.store(screen, available_actions_one_hot, non_spatial_action, spatial_action, observation_new[0].reward, value_t, (non_spatial_logprobability_t, spatial_logprobability_t))
 
         # Update the observation
         observation = observation_new
         screen = np.array(observation[0].observation['feature_screen'], dtype=np.float32)
         screen = np.expand_dims(utils.preprocess_screen(screen), axis=0)
-        available_actions = np.zeros([1, len(actions.FUNCTIONS)], dtype=np.float32)
-        available_actions[0, observation[0].observation['available_actions']] = 1
 
         # Finish trajectory if reached to a terminal state
         terminal = (num_frames >= FLAGS.max_agent_steps) or observation[0].last()
-        if terminal or (t == steps_per_epoch - 1):
-            last_value = 0 if terminal else critic(screen, available_actions)
+        if terminal or (t == FLAGS.steps_per_epoch - 1):
+            last_value = 0 if terminal else critic(screen, available_actions_one_hot)
             buffer.finish_trajectory(last_value)
             sum_return += episode_return
             sum_length += episode_length
@@ -240,16 +199,16 @@ for epoch in range(epochs):
     ) = buffer.get()
 
     # Update the policy and implement early stopping using KL divergence
-    for test in range(train_policy_iterations):
+    for test in range(FLAGS.train_policy_iterations):
         kl = train_policy(
             screen_buffer, available_actions_buffer, non_spatial_action_buffer, spatial_action_buffer, logprobability_buffer, advantage_buffer
         )
-        if kl > 1.5 * target_kl:
+        if kl > 1.5 * FLAGS.target_kl:
             # Early Stopping
             break
 
     # Update the value function
-    for test2 in range(train_value_iterations):
+    for test2 in range(FLAGS.train_value_iterations):
         train_value_function(screen_buffer, available_actions_buffer, return_buffer)
 
     # Print mean return and length for each epoch
